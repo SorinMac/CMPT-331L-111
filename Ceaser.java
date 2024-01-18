0bd54955245c11e75062447bb031c036b028cbe1
SorinMac <sorin.macaluso1@marist.edu>
2024-01-18 14:02:04 -0500

    pointer issue but core concept is there 1/18/23

--- Ceaser.java
+++ Ceaser.java
@@ -1,6 +1,68 @@
+import java.util.Arrays;
+
 public class Ceaser {
+
+    static void encrypt(char[] word, int moveSpace){
+        for(int i=0; i < word.length; i++){
+            int ascii = ((int) word[i] - 'A' + moveSpace) % 26;
+
+            if (ascii < 0) {
+                ascii += 26; 
+            }
+
+            word[i] = (char) (ascii + 'A');
+        }
+
+        System.out.println(Arrays.toString(word) + " This is Encrypted");
+    }
+
+    static void decrypt(char[] word, int moveSpace){
+        for(int i=0; i < word.length; i++){
+            int ascii = ((int) word[i] - 'A' - moveSpace) % 26;
+
+            if (ascii < 0) {
+                ascii += 26; 
+            }
+
+            word[i] = (char) (ascii + 'A');
+        }
+
+        System.out.println(Arrays.toString(word) + " This is Decrypted");
+    }
+
+    static void solve(char[] word){
+        System.out.println("All of them: ");
+
+        for(int i = 0; i < 26; i++){
+            for(int k = 0; k < word.length; k++){
+                int ascii = ((int) word[k] - 'A' + i) % 26;
+
+                if (ascii < 0) {
+                    ascii += 26; 
+                }
+
+                word[k] = (char) (ascii + 'A');
+            }
+
+            System.out.println(Arrays.toString(word));
+        }
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        int move = 5;
+        String test = "SORINZ";
+        char[] word = new char[test.length()];
+
+        for(int i = 0; i < test.length(); i++){
+            word[i] = test.charAt(i);
+        }
+
+        encrypt(word, move);
+        solve(word);
+        decrypt(word, move);
+
+        System.out.println("Done");
+        
     }
     
 }
