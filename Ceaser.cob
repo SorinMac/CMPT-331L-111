56378209ca61a684a2d9baa52f440df4704ceac0
SorinMac <sorin.macaluso1@marist.edu>
2024-01-25 11:55:31 -0500

    1/25/24 I have decided to save cobol for another day

---
+++ Ceaser.cob
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. DISPLAY-STUDENT-INFO. 
+       ENVIRONMENT DIVISION. 
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION. 
+        01 StudentInfo. 
+            03 MyName PIC A(30) VALUE "My Name". 
+            03 MyStudentID PIC X(8) VALUE "FN107001". 
+            03 MyCGPA PIC 9(2)V9(2) VALUE 3.54. 
