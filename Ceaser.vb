b4d481246fbf438c1631ff679fea96efa7277f05
SorinMac <sorinmagnusmacaluso@gmail.com>
2024-01-27 13:15:12 -0500

    1/27/24 BASIC is done

--- Ceaser.vb
+++ Ceaser.vb
@@ -1,22 +1,28 @@
 Function Encrypt(value As String, move As integer) As String
+    'values that i will need to use for thing in this equation
     dim i as integer
     dim ascii as integer
 
     for i = 1 To len(value)
         
         'did it for encrypt just in case
+        'have to seperate it out basic does not like having all the stuff toghether at once
         ascii = (asc(mid(value, i, 1)) - asc("A"))
         ascii = ascii + move
         ascii = ascii Mod 26
-        
+
+        'wrap around stuff
         if ascii < 0 then
             ascii = ascii + 26
         end if
         
+        'turning it back to for the string
         Mid(value, i, 1) = Chr(ascii + Asc("A"))
     
     next i
     
+    'general text stuff
+    print "This is Encrypt"
     print value
     
     return "Done"
@@ -25,6 +31,7 @@ End Function
 
 
 Function Decrypt(value As String, move As integer) As String
+    'values that i will need to use for thing in this equation
     dim i as integer
     dim ascii as integer
 
@@ -35,14 +42,18 @@ Function Decrypt(value As String, move As integer) As String
         ascii = ascii - move
         ascii = ascii Mod 26
         
+        'wrap around stuff
         if ascii < 0 then
             ascii = ascii + 26
         end if
-        
+
+        'general text stuff
         Mid(value, i, 1) = Chr(ascii + Asc("A"))
     
     next i
     
+    'message output stuff
+    print "This is Decrypt"
     print value
     
     return "Done"
@@ -50,8 +61,37 @@ Function Decrypt(value As String, move As integer) As String
 End Function
 
 Function Solve(value As String) As String
+
+    'need two for the double for loop
+    dim i as integer
+    dim k as integer
+    dim ascii as integer
+    dim AS string clone = value
     
-    print "Done"
+    print "This is all of them:"
+    
+    for i = 1 To 25
+        for k = 1 To len(clone)
+
+            'same kind of seperation
+            ascii = (asc(mid(clone, k, 1)) - asc("A"))
+            ascii = ascii + i
+            ascii = ascii Mod 26
+            
+            'wrap around stuff
+            if ascii < 0 then
+                ascii = ascii + 26
+            end if
+
+            'turning it back 
+            Mid(clone, k, 1) = Chr(ascii + Asc("A"))
+        next k
+        
+        'ouput stuff
+        print clone
+        
+    next i
+    
     return "Done"
     
 End Function
@@ -62,6 +102,5 @@ dim AS string test_case = "SORINZ"
 
 'These are the calls to my function for the ceaser cipher
 Encrypt(test_case, move)
+Solve(test_case)
 Decrypt(test_case, move)
-'build out solve
-'Solve(test_case)
