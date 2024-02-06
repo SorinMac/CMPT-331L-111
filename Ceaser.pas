a9cc4b05ca19a2446468d8531497adb1910ce501
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-02-06 13:48:12 -0500

    Update Ceaser.pas

--- Ceaser.pas
+++ Ceaser.pas
@@ -17,7 +17,8 @@ function encrypt(word: string; move: integer): string;
                     
                 word[i] := Chr(ascii + 65);
             end;
-        
+            
+        writeln('This is encrypted');
         writeln(word);
         Exit(word);
     end;
@@ -40,29 +41,56 @@ function decrypt(word: string; move: integer): string;
                 word[i] := Chr(ascii + 65);
             end;
         
+        writeln('This is decrypted');
         writeln(word);
         Exit(word);
     end;
 
 {will do the solve}
 function solve(word: string): string;
-begin
-    writeln('Hello World');
-    Exit('A')
-end;
+    var
+        i: integer;
+        k: integer;
+        ascii: integer;
+    
+    begin
+        writeln('This is the solve');
+        
+        for k := 1 to 26 do
+            begin
+            for i := 1 to length(word) do
+                begin
+                    ascii := (Ord(word[i]) - 65 + k) mod 26;
+                    
+                    if ascii < 0 then 
+                        ascii := ascii + 26;
+                        
+                    word[i] := Chr(ascii + 65);
+                end;
+            
+            writeln(word);
+            
+        end;
+        
+        Exit('A');
+        
+    end;
 
 {declare the variables that we will need}
 var
     word : string;
+    dup : string;
     move : integer;
 
 begin
 
     {declaring the varaibles that we need}
-    word := 'SORINZ';
+    word := 'SORINZ'
+    dup := 'SORINZ'
     move := 5;
     
     word := encrypt(word, move);
+    solve(word);
     decrypt(word, move);
 
 end.
