734e789f6c29008bf585a811a33439faad68ddc6
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-03-14 09:57:56 -0400

    Update Ceaser.fs

--- Ceaser.fs
+++ Ceaser.fs
@@ -1,23 +1,32 @@
-(*used to encrypt the word with return type string*)
+let shiftE (letter, move) =
+    let mutable ascii = ((int(letter) - 65 + move)%26)
+    if ascii < 0 then
+        ascii <- ascii + 26
+    char(ascii + 65)
+    
+let shiftD (letter, move) =
+    let mutable ascii = ((int(letter) - 65 - move)%26)
+    if ascii < 0 then
+        ascii <- ascii + 26
+    char(ascii + 65)
+
+    
 let Encrypt (word : string) (move : int) : string =
-    printfn "%s" word
-    printfn "%i" move
-    (*how to return a specific value*)
-    let test = "balls"
-    test
+    printfn "%s" "This is the Encryption: "
+    let shifted = String.map (fun i -> shiftE(i, move)) word
+    printfn "%s" shifted
+    shifted
         
 let Decrypt (word : string) (move : int) =
-    printfn "%s" word
-    printfn "%i" move
+    printfn "%s" "This is the Decryption: "
+    let shifted = String.map (fun i -> shiftD(i, move)) word
+    printfn "%s" shifted
     
 let Solve (word : string) (move : int) =
     printfn "%s" word
     printfn "%i" move
 
-(*have to make mutable so I can it*)
 let mutable word = "SORINZ"
 let move = 5
-
-(*testing to make sure the return is correct*)
 word <- Encrypt word move
-printfn "%s" word
+Decrypt word move
