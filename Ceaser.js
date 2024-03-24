e1db00f720386d6cecee253335506f8e82882946
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-03-24 12:15:16 -0400

    Create Ceaser.js

---
+++ Ceaser.js
@@ -0,0 +1,18 @@
+function encrypt(word, move){
+    console.log(word);
+    console.log(move);
+}
+
+function decrypt(word, move){
+    console.log(word);
+    console.log(move);
+}
+
+function solve(word, move){
+    console.log(word);
+    console.log(move);
+}
+
+test = "SORINZ";
+move = 5;
+word = test.split('');
