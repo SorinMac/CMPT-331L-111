4befa33fb0896c130037fa9238ecfb14cbe0f04d
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-03-24 12:51:06 -0400

    Update Ceaser.js

--- Ceaser.js
+++ Ceaser.js
@@ -32,7 +32,26 @@ function decrypt(word, move, i){
 }
 
 function solve(word, move, i){
+    test = "SORINZ";
+    clone = test.split('');
     
+    if (move < 27){
+       if(i < (word.length)){
+            ascii = ((word[i].charCodeAt(0) - 65 + move) % 26);
+        
+            if(ascii < 0){
+                ascii = ascii + 26;
+            }
+            
+            word[i] = String.fromCharCode(ascii + 65);
+            
+            solve(word, move, i+1);
+       }else{
+           console.log(word)
+           
+           solve(clone, move+1, 0)
+       }
+    }
 }
 
 test = "SORINZ";
@@ -42,8 +61,12 @@ clone = test.split('');
 
 console.log("This is Encrypted: ");
 clone = encrypt(word, move, 0);
-console.log(word);
+clone2 = structuredClone(clone);
+console.log(clone);
+
+console.log("This is Solved: ");
+solve(word, 1, 0);
 
 console.log("This is Decrypted: ");
-decrypt(clone, move, 0);
-console.log(word);
+decrypt(clone2, move, 0);
+console.log(clone2);
