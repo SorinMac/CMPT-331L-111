df1aa1755b4bb5bd70eebff9cdd2c04ae3d4fedc
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-03-24 12:37:00 -0400

    Update Ceaser.js

--- Ceaser.js
+++ Ceaser.js
@@ -1,18 +1,49 @@
-function encrypt(word, move){
-    console.log(word);
-    console.log(move);
+function encrypt(word, move, i){
+    if(i < word.length){
+        ascii = ((word[i].charCodeAt(0) - 65 + move) % 26);
+        
+        if(ascii < 0){
+            ascii = ascii + 26;
+        }
+        
+        word[i] = String.fromCharCode(ascii + 65);
+        
+        encrypt(word, move, i+1);
+        
+    }
+    
+    return word;
 }
 
-function decrypt(word, move){
-    console.log(word);
-    console.log(move);
+function decrypt(word, move, i){
+    if(i < word.length){
+        ascii = ((word[i].charCodeAt(0) - 65 - move) % 26);
+        
+        if(ascii < 0){
+            ascii = ascii + 26;
+        }
+        
+        word[i] = String.fromCharCode(ascii + 65);
+        
+        decrypt(word, move, i+1);
+        
+    }
+    
 }
 
-function solve(word, move){
-    console.log(word);
-    console.log(move);
+function solve(word, move, i){
+    
 }
 
 test = "SORINZ";
 move = 5;
 word = test.split('');
+clone = test.split('');
+
+console.log("This is Encrypted: ");
+clone = encrypt(word, move, 0);
+console.log(word);
+
+console.log("This is Decrypted: ");
+decrypt(clone, move, 0);
+console.log(word);
