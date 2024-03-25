4a86171a3bf7b4a6c77ee5ea0019ae3d0019a1a5
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-03-25 16:08:42 -0400

    Update Ceaser.erl

--- Ceaser.erl
+++ Ceaser.erl
@@ -1,9 +1,19 @@
 -module(prog).
 -export([main/0]).
 
-encrypt(Test,Move) ->
-	io:fwrite("~p~n",[Test]),
-	io:fwrite("~p~n",[Move]).
+shift(Char, Move) ->
+	Ascii = (hd(Char) - 65 + Move) rem (26),
+	if
+		Ascii < 0 ->
+			Ascii = Ascii + 26;
+		true ->
+			Ascii
+	end,
+	integer_to_list(Ascii + 65).
+
+encrypt(Test, Move) ->
+    Encrypted = lists:map(fun(Char) -> shift([Char], Move) end, Test),
+    io:fwrite("~p~n", [Encrypted]).
 	
 decrypt(Test,Move) ->
 	io:fwrite("~p~n",[Test]),
@@ -16,6 +26,4 @@ solve(Test,Move) ->
 main() ->
 	Test = "SORINZ",
 	Move = 5,
-	encrypt(Test, Move),
-	decrypt(Test, Move),
-	solve(Test, Move).
+	encrypt(Test, Move).
