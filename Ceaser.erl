027233e5d4386cf53ce6da2772fdd889e463c182
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-03-25 15:26:37 -0400

    Update Ceaser.erl

--- Ceaser.erl
+++ Ceaser.erl
@@ -1,5 +1,21 @@
 -module(prog).
 -export([main/0]).
 
+encrypt(Test,Move) ->
+	io:fwrite("~p~n",[Test]),
+	io:fwrite("~p~n",[Move]).
+	
+decrypt(Test,Move) ->
+	io:fwrite("~p~n",[Test]),
+	io:fwrite("~p~n",[Move]).
+	
+solve(Test,Move) ->
+	io:fwrite("~p~n",[Test]),
+	io:fwrite("~p~n",[Move]).
+	
 main() ->
-	io:fwrite("Hello, world!\n").
+	Test = "SORINZ",
+	Move = 5,
+	encrypt(Test, Move),
+	decrypt(Test, Move),
+	solve(Test, Move).
