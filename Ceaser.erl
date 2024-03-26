2d692f53db9b01e3a44a1a41e1702d3fcdfa049a
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-03-26 11:52:53 -0400

    Update Ceaser.erl

--- Ceaser.erl
+++ Ceaser.erl
@@ -1,29 +1,50 @@
 -module(prog).
 -export([main/0]).
 
-shift(Char, Move) ->
-	Ascii = (hd(Char) - 65 + Move) rem (26),
+shift1(Char, Move) ->
+	Ascii = (hd(Char) - $A + Move) rem (26),
 	if
 		Ascii < 0 ->
 			Ascii = Ascii + 26;
 		true ->
 			Ascii
 	end,
-	integer_to_list(Ascii + 65).
+	Ascii + $A.
 
+shift2(Char, Move) ->
+	Ascii = (hd(Char) - $A - Move) rem (26),
+	if
+		Ascii < 0 ->
+			Ascii = Ascii + 26;
+		true ->
+			Ascii
+	end,
+	Ascii + $A.
+	
 encrypt(Test, Move) ->
-    Encrypted = lists:map(fun(Char) -> shift([Char], Move) end, Test),
+    Encrypted = lists:map(fun(Char) -> shift1([Char], Move) end, Test),
     io:fwrite("~p~n", [Encrypted]).
 	
-decrypt(Test,Move) ->
-	io:fwrite("~p~n",[Test]),
-	io:fwrite("~p~n",[Move]).
+decrypt(Test, Move) ->
+	Decrypt = lists:map(fun(Char) -> shift2([Char], Move) end, Test),
+    io:fwrite("~p~n", [Decrypt]).
 	
-solve(Test,Move) ->
-	io:fwrite("~p~n",[Test]),
-	io:fwrite("~p~n",[Move]).
+solve(Test, Move, Place) ->
+	Solve = lists:map(fun(Char) -> shift1([Char], Move) end, Test),
+	io:fwrite("~p~n",[Solve]),
+	if
+		Place < 25 ->
+			solve("SORINZ", Move+1, Place+1);
+		true ->
+			true
+	end.
 	
 main() ->
 	Test = "SORINZ",
 	Move = 5,
-	encrypt(Test, Move).
+	io:fwrite("This is Encrypt:\n"),
+	encrypt(Test, Move),
+	io:fwrite("This is Solve:\n"),
+	solve(Test, 1, 0),
+	io:fwrite("This is Decrypt:\n"),
+	decrypt(Test, Move).
